72c0ee35dadab071fe4ca8bbd0caab9c947a3ed6
Mohammad Partonia <partonia.m@gmail.com>
2025-08-18 15:37:53 +0330

    Feat(FileStorage):Modular FileStorage Architecture with Independent NuGet Packages #1

---
+++ Bulud.FileStorage.Local/LocalFileService.cs
@@ -0,0 +1,44 @@
+﻿using Bulud.Base.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Bulud.FileStorage.Local;
+
+public class LocalFileService(IWebHostEnvironment env) : IFilesService
+{
+    public async Task<string> Upload(IFormFile file, string destPath, string fileName)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", destPath);
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        var filePath = Path.Combine(path, fileName);
+
+        await using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return $"/{destPath}/{fileName}";
+    }
+
+    public Task<(Stream stream, string contentType)> Download(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task Move(string sourcePath, string destPath)
+    {
+        var sPath = Path.Combine(env.WebRootPath, sourcePath.TrimStart('/'));
+        var dPath = Path.Combine(env.WebRootPath, destPath.TrimStart('/'));
+        File.Move(sPath, dPath);
+        return Task.CompletedTask;
+    }
+}
---
+++ Bulud.FileStorage.S3/S3FileService.cs
@@ -0,0 +1,76 @@
+﻿using System.Web;
+using Bulud.Base.Extensions;
+using Bulud.Base.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Minio;
+using Minio.DataModel.Args;
+
+namespace Bulud.FileStorage.S3;
+
+public class S3FileService(IMinioClient minio, IOptions<MinioSettings> settings) : IFilesService
+{
+    public async Task<string> Upload(IFormFile file, string destPath, string fileName)
+    {
+        var stream = file.OpenReadStream();
+        var obj = $"{destPath.Replace('\\', '/')}/{fileName}";
+        await minio.PutObjectAsync(new PutObjectArgs()
+            .WithBucket(settings.Value.Bucket)
+            .WithObject(obj)
+            .WithStreamData(stream)
+            .WithObjectSize(file.Length)
+            .WithContentType(file.ContentType));
+        return obj;
+    }
+
+    public async Task<(Stream stream, string contentType)> Download(string filePath)
+    {
+        var outputStream = new MemoryStream();
+        filePath = filePath.Replace("\\", "/");
+        await minio.GetObjectAsync(new GetObjectArgs()
+            .WithBucket(settings.Value.Bucket)
+            .WithObject(HttpUtility.UrlDecode(filePath))
+            .WithCallbackStream(stream => { stream.CopyTo(outputStream); }));
+
+        outputStream.Position = 0;
+        var contentType = filePath.GetMimeType();
+
+        return (outputStream, contentType);
+    }
+
+
+    public async Task Delete(string filePath)
+    {
+        filePath = filePath.Replace("\\", "/");
+        await minio.RemoveObjectAsync(new RemoveObjectArgs()
+            .WithBucket(settings.Value.Bucket)
+            .WithObject(filePath));
+    }
+
+    public async Task Move(string sourcePath, string destPath)
+    {
+        sourcePath = sourcePath.Replace("\\", "/");
+        destPath = destPath.Replace("\\", "/");
+        var copySource = new CopySourceObjectArgs()
+            .WithBucket(settings.Value.Bucket)
+            .WithObject(sourcePath);
+
+        await minio.CopyObjectAsync(new CopyObjectArgs()
+            .WithBucket(settings.Value.Bucket)
+            .WithObject(destPath)
+            .WithCopyObjectSource(copySource));
+
+        await minio.RemoveObjectAsync(new RemoveObjectArgs()
+            .WithBucket(settings.Value.Bucket)
+            .WithObject(destPath));
+    }
+}
+
+public class MinioSettings
+{
+    public string Endpoint { get; set; } = string.Empty;
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public required string Bucket { get; set; }
+    public bool UseSSL { get; set; }
+}
